e486f52ee66fd7caee5ff74d439441aa26aba004
jonghun <minn4072@gmail.com>
2023-06-05 00:24:41 +0900

    feat: footer 컴포넌트 Layout 컴포넌트로 반영

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
+import Footer from './common/Footer';
 
 function Layout() {
   return (
     <section className="bg-baskin-robbins-background max-w-md h-screen m-auto">
       <Outlet />
+      <Footer />
     </section>
   );
 }
