75c24e35d19e539faeb6d4ad3f134a77635c2dbe
jonghun <minn4072@gmail.com>
2023-06-05 02:47:41 +0900

    feat: quiz.ts 정의, quiz.tsx에 적용

---
+++ src/types/quiz.ts
@@ -0,0 +1,11 @@
+export type QUIZ_LIST_TYPE = {
+  [key: string]: {
+    quizId: number;
+    type: string;
+    points: number;
+    question: string;
+    imageUrl: string;
+    choice: { id: number; option: string }[];
+    answer: number;
+  };
+};
