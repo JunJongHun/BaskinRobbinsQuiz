0df822d9993c6c0b2cc8e519c2ef7a05358a2bbb
JunJongHun <90402926+JunJongHun@users.noreply.github.com>
2023-06-06 01:54:18 +0900

    feat: 퀴즈 페이지 구현

--- src/pages/QuizPage.tsx
+++ src/pages/QuizPage.tsx
@@ -1,8 +1,26 @@
 import { useState } from 'react';
 import { QUIZ_MAX_COUNT, QUIZ_LIST } from '../constants/quiz';
+import { calculateScore, getTypeToKorean } from '../utils/quiz';
+import Timer from '../components/Timer';
+import { UserAnswerType } from '../types/quiz';
 
 function QuizPage() {
   const [page, setPage] = useState(1);
+  const { type, question, points, imageUrl, choice } = QUIZ_LIST[page];
+  const [userAnswer, setUserAnswer] = useState<UserAnswerType>({
+    A1: '',
+    A2: '',
+    A3: '',
+    A4: '',
+    A5: '',
+    A6: '',
+    A7: '',
+    A8: '',
+    A9: '',
+    A10: '',
+    A11: '',
+    A12: '',
+  });
 
   const prePage = () => setPage(page - 1);
   const nextPage = () => setPage(page + 1);
@@ -11,28 +29,45 @@ function QuizPage() {
     <section className="flex flex-col items-center gap-8 pt-32 px-4 ">
       <article className="flex flex-col gap-6">
         <h3>2023년 베스킨라빈스 능력 시험</h3>
-        <h1 className="text-3xl m-auto">{QUIZ_LIST[page].type}</h1>
+        <h1 className="text-3xl m-auto">{getTypeToKorean(type)}</h1>
       </article>
-      <article className="flex flex-col w-full h-8 justify-between">
+      <article className="flex flex-col w-full h-8  justify-between">
         <div className="flex justify-between">
-          <span>14 : 51</span>
-          <div>{QUIZ_MAX_COUNT} 문제</div>
+          <span></span>
+          <div className="border border-black py-1 px-2 rounded-2xl">
+            {QUIZ_MAX_COUNT} 문제
+          </div>
         </div>
-        <progress className="w-full " value="50" max="100" />
+        <Timer />
       </article>
-      <div className="border border-black w-full"></div>
-      <article className="">
+      {/* <div className="border border-black w-full"></div> */}
+      <article className="mt-4">
         <div>
-          {page}. {QUIZ_LIST[page].question} [{QUIZ_LIST[page].points}점]
+          {page}. {question} [{points}점]
         </div>
-        <img
-          className="m-auto"
-          src={QUIZ_LIST[page].imageUrl}
-          alt="문제 이미지"
-        />
-        <ul className=" list-decimal">
-          {QUIZ_LIST[page].choice.map(({ id, option }) => (
-            <li key={id}>{option}</li>
+        <img className="m-auto my-4 py-2" src={imageUrl} alt="문제 이미지" />
+        <ul className=" grid grid-cols-2 gap-2">
+          {choice.map(({ id, option }, index) => (
+            <li
+              className="flex items-center"
+              key={id}
+              onClick={() => {
+                const newUserAnswer = { ...userAnswer };
+                newUserAnswer[`A${page}`] = String(index + 1);
+                setUserAnswer(newUserAnswer);
+              }}
+            >
+              <span className=" relative flex justify-center items-center w-5 h-5 rounded-full border border-black mr-1">
+                {index + 1}
+                {userAnswer['A' + page] === String(index + 1) && (
+                  <img
+                    className=" absolute translate-x-1/4 -translate-y-1/2  w-8 h-6"
+                    src="/src/assets/check.png"
+                  />
+                )}
+              </span>
+              {option}
+            </li>
           ))}
         </ul>
       </article>
@@ -47,13 +82,24 @@ function QuizPage() {
         <div>
           {page} / {QUIZ_MAX_COUNT}
         </div>
-        <button
-          className=" bg-gray-300 w-20 h-8 text-gray-500 rounded-3xl disabled:invisible"
-          onClick={nextPage}
-          disabled={page === QUIZ_MAX_COUNT}
-        >
-          다음
-        </button>
+        {page !== QUIZ_MAX_COUNT ? (
+          <button
+            className=" bg-gray-300 w-20 h-8 text-gray-500 rounded-3xl disabled:invisible"
+            onClick={nextPage}
+            disabled={page === QUIZ_MAX_COUNT}
+          >
+            다음
+          </button>
+        ) : (
+          <button
+            className=" bg-pink-500 w-20 h-8  text-white rounded-3xl "
+            onClick={() => {
+              console.log(calculateScore(userAnswer, QUIZ_LIST));
+            }}
+          >
+            제출
+          </button>
+        )}
       </article>
     </section>
   );
