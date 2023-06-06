de6b1fd51e387a785e98b8e8d87334e1b1404284
jonghun <minn4072@gmail.com>
2023-06-06 21:36:27 +0900

    feat: msw 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router.tsx';
+import { worker } from './mocks/browser';
 import './reset.css';
+
+if (process.env.NODE_ENV === 'development') worker.start();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
