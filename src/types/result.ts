3a6b932d65f8c11443d75cba74134cc2bef383e3
jonghun <minn4072@gmail.com>
2023-06-06 21:37:27 +0900

    feat: type 정의

--- src/types/result.ts
+++ src/types/result.ts
@@ -3,3 +3,11 @@ export type RatingType = { grade: number };
 export type RatingRangeType = { grade: number; start: number; end: number };
 
 export type RatingPercentageType = { percentage: number };
+
+export type AvgScoreType = {
+  total_avg: number;
+  vocabulary_avg: number;
+  cogitation_avg: number;
+  intelligence_avg: number;
+  observation_avg: number;
+};
