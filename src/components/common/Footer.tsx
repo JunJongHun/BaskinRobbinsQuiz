c5f726b06966fe8962572949756535173e8701ed
jonghun <minn4072@gmail.com>
2023-06-11 01:36:55 +0900

    !HOTFIX: 이미지 처리 수정

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,4 +1,6 @@
 import { useNavigate } from 'react-router-dom';
+import BaskinRobbinsTitle from '../../assets/BaskinRobbinsTitle.png';
+import Icon from '../../assets/Icon.png';
 
 function Footer() {
   const navigate = useNavigate();
@@ -8,10 +10,10 @@ function Footer() {
       className="flex justify-center mt-8 h-32 gap-4 -ml-4 hover:cursor-pointer"
       onClick={() => navigate('/')}
     >
-      <img className="w-7 h-7" src="/src/assets/Icon.png" alt="베라 아이콘" />
+      <img className="w-7 h-7" src={Icon} alt="베라 아이콘" />
       <img
         className=" w-48 h-6"
-        src="/src/assets/BaskinRobbinsTitle.png"
+        src={BaskinRobbinsTitle}
         alt="베라 영문 이름"
       />
     </article>
